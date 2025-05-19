62f437f98324ed4ab61de849736707aa87e716d3
bastosrafa <bastosrafael33@gmail.com>
2025-05-19 16:01:24 -0300

    Ajuste layout report a problem e profile

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ptcg7jlpa3"
+    "revision": "0.dt1flrck3qg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
